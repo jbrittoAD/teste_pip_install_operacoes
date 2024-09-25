2005ef9945ea838145d29f9d835c64020e05077e
jbrittoAD <jbritto@universo.univates.br>
2024-09-25 13:21:06 -0300

    comit 1

--- operacoes/setup.py
+++ operacoes/setup.py
@@ -4,10 +4,10 @@ setup(
     name="operacoes",
     version="0.1",
     py_modules=["operacoes"],
-    description="Pacote para opera��es aritm�ticas b�sicas",
+    description="Pacote para operacoes aritmeticas basicas",
     author="Seu Nome",
-    author_email="teste@example.com",
-    url="https://github.com/seuusuario/operacoes",
+    author_email="jbritto@universo.univates.br",
+    url="https://github.com/jbrittoAD/teste_pip_install_operacoes",
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
